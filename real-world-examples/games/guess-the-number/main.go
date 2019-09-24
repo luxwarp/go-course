9cdca2f81aa78be069d95468d7240aec3b55b978
luxwarp <mikael.m.carlsson@gmail.com>
2019-09-24 22:12:19 +0200

    added guess the number game

---
+++ real-world-examples/games/guess-the-number/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const minNumber int = 1
+
+var maxNumber int
+var randomNumber int
+
+func main() {
+	fmt.Println("Guess the number")
+	fmt.Printf("Enter a number above 0 that you want to guess for: ")
+	fmt.Scanln(&maxNumber)
+	fmt.Println("Okey, now it's time to guess what number between 0 and", maxNumber, "that I have chosen.")
+	rand.Seed(time.Now().UnixNano())
+
+	randomNumber = rand.Intn(maxNumber-minNumber+1) + minNumber
+
+	var guessedNumber int
+	for guessedNumber != randomNumber {
+		fmt.Printf("Guess: ")
+		fmt.Scanln(&guessedNumber)
+
+		if guessedNumber < randomNumber {
+			fmt.Println("To low. Try again!")
+		} else if guessedNumber > randomNumber {
+			fmt.Println("To high. Try again!")
+		} else {
+			fmt.Println("Nice job! That correct. You win!")
+			return
+		}
+	}
+}
